3ba5b1f9744be6b29c3c2b414bfd08538900f592
waremon <waremon.mail@gmail.com>
2013-11-29 06:26:09 +0900

    sort taged images

--- development-01/FirstViewController.m
+++ development-01/FirstViewController.m
@@ -60,11 +60,23 @@
     scrollView.frame = self.view.bounds;
 
     // get all kind of taged id
-    NSMutableArray *tagIds = [self getAllTagedIds];
+    //NSMutableArray *tagIds = [self getAllTagedIds];
+    
+    // get all taged id sorted by saved_id
+    NSMutableDictionary *tagIds = [self getAllTagedIds];
+    
+    NSArray *testArray = [tagIds allKeys];
+    for(NSNumber *test in testArray) {
+        NSLog(@"test:%@", test);
+    }
+    
+    NSArray *tagCreatedArray = [[tagIds allKeys] sortedArrayUsingSelector:@selector(compare:)];
 
     // get newest image of each tag
     NSMutableArray *eachTagImageInfo = [[NSMutableArray alloc] init];
-    for (NSNumber *tagId in tagIds) {
+    for (NSNumber *createdAt in tagCreatedArray) {
+        NSLog(@"createdat:%@", createdAt);
+        NSNumber *tagId = [tagIds objectForKey:createdAt];
         NSDictionary *imageInfo = [self getEachTagImageInfo:tagId];
         [eachTagImageInfo addObject:imageInfo];
     }
@@ -77,22 +89,43 @@
         UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
         imageView.tag = [[unit objectForKey:@"image_id"] intValue];
         imageView.userInteractionEnabled = YES;
+        NSNumber *tag_id = [unit objectForKey:@"tag_id"];
         
         int x,y;
         x = ((count % 3) * 100) + 10;
-        y = ((count / 3) * 100) + 10;
+        y = ((count / 3) * 110) + 10;
         
         imageView.frame = CGRectMake(x, y, 90, 90);
         
         [scrollView insertSubview:imageView atIndex:[self.view.subviews count]];
+        
+        UILabel *tagLabel = [[UILabel alloc] initWithFrame:CGRectMake(x, y + 90, 90, 20)];
+        tagLabel.textColor = [UIColor blueColor];
+        tagLabel.font = [UIFont fontWithName:@"AppleGothic" size:12];
+        tagLabel.textAlignment = NSTextAlignmentCenter;
+        //TODO: it should be in module, tag name from tag_id;
+        tagLabel.text = [self getTagNameById:tag_id];
+        [scrollView insertSubview:tagLabel atIndex:[self.view.subviews count]];
         count++;
-        
-        
     }
     // add scrollView
     NSInteger heightCount = floor(count / 3) + 1;
     scrollView.contentSize = CGSizeMake(320, (120 * heightCount));
     [self.view addSubview:scrollView];
+}
+
+- (NSMutableArray*)getTagNameById:(NSNumber*)tagId{
+    NSString *stmt = @"select tag_name from tags where id = ?;";
+    
+    DA *da = [DA da];
+    [da open];
+    FMResultSet *results = [da executeQuery:stmt, tagId];
+    NSString *tagName = [[NSString alloc] init];
+    while ([results next]) {
+        tagName = [NSString stringWithString:[results stringForColumn:@"tag_name"]];
+    }
+    [da close];
+    return tagName;
 }
 
 - (NSMutableArray*)getAllTagName{
@@ -118,12 +151,19 @@
     DA *da = [DA da];
     [da open];
     FMResultSet *results = [da executeQuery:stmt];
+    NSMutableDictionary *tagInfo = [[NSMutableDictionary alloc] init];
     while ([results next]) {
-        NSNumber *id = [NSNumber numberWithInt:[results intForColumn:@"tag_id"]];
-        [tagId addObject:id];
+        NSNumber *tag_id = [NSNumber numberWithInt:[results intForColumn:@"tag_id"]];
+        NSString *stmt2 = @"select created_at from tag_map where tag_id = ? order by created_at desc limit 1;";
+        FMResultSet *results2 = [da executeQuery:stmt2, tag_id];
+        NSNumber *created_at = [[NSNumber alloc] init];
+        while ([results2 next]) {
+            created_at = [NSNumber numberWithInt:[results2 intForColumn:@"created_at"]];
+        }
+        [tagInfo setObject:tag_id forKey:created_at];
     }
     [da close];
-    return tagId;
+    return tagInfo;
 }
 
 - (NSDictionary*)getEachTagImageInfo:(NSNumber*)tagId {
